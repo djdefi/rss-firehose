0f5901d32e3dca192f8130df5bfa6e481287519f
Ryan Trauntvein <djdefi@github.com>
2017-09-12 09:40:28 -0700

    dont rescue exceptions

--- consume.rb
+++ consume.rb
@@ -1,24 +1,25 @@
 require 'rss'
 require 'httparty'
 
+# Read list of URLs from file
 File.foreach('urls.txt') do |url|
-  begin
-    url.chomp!
-    url = URI.encode(url)
-    response = HTTParty.get(url, timeout: 60)
-    feed = RSS::Parser.parse(response.body, do_validate = false)
-    puts '<hr>'
-    puts "<a href='#{url}'>#{url} - #{feed.items.count} items:</a>"
-    puts '<ul>'
+    # Remove extra characters and URI encode
+  url.chomp!
+  url = URI.encode(url)
 
-    feed.items.each do |item|
-      puts "<li><a href='#{item.link}'>#{item.title}</a></li>"
-    end
-  rescue StandardError => e
-    puts
-    puts "ERROR: Had trouble parsing #{url} !"
-    puts e.message
-    puts e.backtrace.inspect
+  # Get the feeds and parse them. We don't validate because some feeds are
+  # malformed slightly and break the parser.
+  response = HTTParty.get(url, timeout: 60)
+  feed = RSS::Parser.parse(response.body, do_validate = false)
+
+  # Feed headers HTML
+  puts '<hr>'
+  puts "<a href='#{url}'>#{url} - #{feed.items.count} items:</a>"
+  puts '<ul>'
+
+  # Items listing in HTML
+  feed.items.each do |item|
+    puts "  <li><a href='#{item.link}'>#{item.title}</a></li>"
   end
-  puts '</ul>'
 end
+puts '</ul>'
