34352f2cbeeda5e08d5ef9207dc33a5b3b49b5a5
Ryan Trauntvein <djdefi@github.com>
2017-11-14 19:26:16 -0800

    Write html directly

--- consume.rb
+++ consume.rb
@@ -2,25 +2,29 @@
 require 'rss'
 require 'httparty'
 
-# Read list of URLs from file
-File.foreach('urls.txt') do |url|
-  # Remove extra characters and URI encode
-  url.chomp!
-  url = URI.encode(url)
+File.open('index.html', 'w') do |fo|
 
-  # Get the feeds and parse them. We don't validate because some feeds are
-  # malformed slightly and break the parser.
-  response = HTTParty.get(url, timeout: 60)
-  feed = RSS::Parser.parse(response.body, _do_validate = false)
 
-  # Feed headers HTML
-  puts '<hr>'
-  puts "<a href='#{url}'>#{url} - #{feed.items.count} items:</a>"
-  puts '<ul>'
+  # Read list of URLs from file
+  File.foreach('urls.txt') do |url|
+    # Remove extra characters and URI encode
+    url.chomp!
+    url = URI.encode(url)
 
-  # Items listing in HTML
-  feed.items.each do |item|
-    puts "  <li><a href='#{item.link}'>#{item.title}</a></li>"
+    # Get the feeds and parse them. We don't validate because some feeds are
+    # malformed slightly and break the parser.
+    response = HTTParty.get(url, timeout: 60)
+    feed = RSS::Parser.parse(response.body, _do_validate = false)
+
+    # Feed headers HTML
+    fo.puts '<hr>'
+    fo.puts "<a href='#{url}'>#{url} - #{feed.items.count} items:</a>"
+    fo.puts '<ul>'
+
+    # Items listing in HTML
+    feed.items.each do |item|
+      fo.puts "  <li><a href='#{item.link}'>#{item.title}</a></li>"
+    end
+    fo.puts '</ul>'
   end
-  puts '</ul>'
 end
