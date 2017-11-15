993292dce53e798f384aba97900d94abee106324
Ryan Trauntvein <djdefi@github.com>
2017-11-14 19:17:40 -0800

    rubocop fixes

--- consume.rb
+++ consume.rb
@@ -1,16 +1,17 @@
+#!/usr/bin/env ruby
 require 'rss'
 require 'httparty'
 
 # Read list of URLs from file
 File.foreach('urls.txt') do |url|
-    # Remove extra characters and URI encode
+  # Remove extra characters and URI encode
   url.chomp!
   url = URI.encode(url)
 
   # Get the feeds and parse them. We don't validate because some feeds are
   # malformed slightly and break the parser.
   response = HTTParty.get(url, timeout: 60)
-  feed = RSS::Parser.parse(response.body, do_validate = false)
+  feed = RSS::Parser.parse(response.body, _do_validate = false)
 
   # Feed headers HTML
   puts '<hr>'
