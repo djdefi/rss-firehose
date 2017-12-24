1b210521910347d20e2c1b9abef8d7b97f49bf0f
Ryan Trauntvein <djdefi@github.com>
2017-12-24 15:25:53 -0800

    Dockerize and sinatrafy

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM ruby:2.4-onbuild
+CMD ["./consume.rb"]
--- consume.rb
+++ consume.rb
@@ -1,28 +1,40 @@
 #!/usr/bin/env ruby
 require 'rss'
 require 'httparty'
+require 'sinatra'
 
-File.open('index.html', 'w') do |fo|
-  # Read list of URLs from file
-  File.foreach('urls.txt') do |url|
-    # Remove extra characters and URI encode
-    url.chomp!
-    url = URI.encode(url)
+def grabrss
+  File.open('public/index.html', 'w') do |fo|
+    # Read list of URLs from file
+    File.foreach('urls.txt') do |url|
+      # Remove extra characters and URI encode
+      url.chomp!
+      url = URI.encode(url)
 
-    # Get the feeds and parse them. We don't validate because some feeds are
-    # malformed slightly and break the parser.
-    response = HTTParty.get(url, timeout: 60)
-    feed = RSS::Parser.parse(response.body, _do_validate = false)
+      # Get the feeds and parse them. We don't validate because some feeds are
+      # malformed slightly and break the parser.
+      response = HTTParty.get(url, timeout: 60)
+      feed = RSS::Parser.parse(response.body, _do_validate = false)
 
-    # Feed headers HTML
-    fo.puts '<hr>'
-    fo.puts "<a href='#{url}'>#{url} - #{feed.items.count} items:</a>"
-    fo.puts '<ul>'
+      # Feed headers HTML
+      fo.puts '<hr>'
+      fo.puts "<a href='#{url}'>#{url} - #{feed.items.count} items:</a>"
+      fo.puts '<ul>'
 
-    # Items listing in HTML
-    feed.items.each do |item|
-      fo.puts "  <li><a href='#{item.link}'>#{item.title}</a></li>"
+      # Items listing in HTML
+      feed.items.each do |item|
+        fo.puts "  <li><a href='#{item.link}'>#{item.title}</a></li>"
+      end
+      fo.puts '</ul>'
     end
-    fo.puts '</ul>'
   end
+end
+
+get '/' do
+  grabrss
+  html :index
+end
+
+def html(view)
+  File.read(File.join('public', "#{view}.html"))
 end
