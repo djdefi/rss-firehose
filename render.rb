6e8bf19c37423f17885d5a430027ffa10241678a
Ryan Trauntvein <djdefi@github.com>
2018-05-09 00:25:58 -0700

    Switch to erb based rendering

---
+++ render.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+require 'erb'
+require 'rss'
+require 'httparty'
+
+def render
+  html = File.open('index.html.erb').read
+  template = ERB.new(html, nil, '-')
+  template.result
+  File.open('public/index.html', 'w') do |fo|
+    fo.puts template.result
+  end
+end
+
+def rss_urls
+  File.readlines('urls.txt').map(&:chomp)
+end
+
+# Get the feeds and parse them. We don't validate because some feeds are
+# malformed slightly and break the parser.
+def feed(url)
+  response = HTTParty.get(url, timeout: 60)
+  RSS::Parser.parse(response.body, _do_validate = false)
+end
+
+def title
+  'News Firehose'
+end
+
+render
